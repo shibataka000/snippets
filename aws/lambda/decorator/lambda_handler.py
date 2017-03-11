a253f6deaf48632da3cbecd2f93d8dbca59f1df7
shibataka000 <chise.alter.pasta@gmail.com>
2017-03-11 22:40:33 +0900

    Add snippets about how to modify parameter passed by Lambda.

--- aws/lambda/decorator/lambda_handler.py
+++ aws/lambda/decorator/lambda_handler.py
@@ -1,11 +1,17 @@
 # coding: utf-8
 
+import json
+
 
 def decorator1(value):
     def receive_func(func):
         def wrapper(*args, **kwargs):
             print("Decorator1: value={}".format(value))
-            return func(*args, **kwargs)
+            (event, context) = args
+            if event is None:
+                event = {}
+            event.update({"Decorator1": value})
+            return func(event, context)
         return wrapper
     return receive_func
 
@@ -22,7 +28,7 @@ def decorator2(value):
 @decorator1("hoge")
 @decorator2("fuga")
 def run(event, context):
-    print("run")
+    print("Run: event={}".format(json.dumps(event)))
 
 
 if __name__ == "__main__":
