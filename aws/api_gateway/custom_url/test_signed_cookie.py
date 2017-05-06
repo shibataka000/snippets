999c0994783e14e3e1b297a99fb5054afbc76b62
shibataka000 <chise.alter.pasta@gmail.com>
2017-05-06 21:50:22 +0900

    Revert "Rename /aws/api_gateway/custom_url to /aws/cloudfront/multi_origins."
    This reverts commit b40e662ee4fd9d6bafd598866999894ac29208d8.

---
+++ aws/api_gateway/custom_url/test_signed_cookie.py
@@ -0,0 +1,85 @@
+# coding: utf-8
+
+'''
+How to run test.
+
+1. Generate CloudFront key pair and save secret key.
+2. Set following parameter in this script.
+    - CLOUDFRONT_KEY_PATH: Path to CloudFront secret key.
+    - CLOUDFRONT_KEY_PAIR_ID: CloudFront key pair id
+    - CLOUDFRONT_URL: CloudFront URL (e.g. http://dunfj7ei6w49.cloudfront.net).
+3. Run pytest.
+'''
+
+import requests
+import json
+import subprocess
+import datetime
+import time
+
+
+CLOUDFRONT_KEY_PATH = './sk.pem'
+CLOUDFRONT_KEY_PAIR_ID = 'APKAJEL5RQQ36NTUXW6Q'
+CLOUDFRONT_URL = 'http://dunfj7ei6w49.cloudfront.net'
+
+
+def get_cloudfront_policy(url, expires):
+    policy = {
+        'Statement': [
+            {
+                'Resource': url,
+                'Condition': {'DateLessThan': {'AWS:EpochTime': expires}},
+            }
+        ]
+    }
+    return json.dumps(policy).replace(' ', '')
+
+
+def get_cloudfront_policy_base64(url, expires):
+    policy = get_cloudfront_policy(url, expires)
+    cmd = ('echo \'{0}\' | tr -d "\\n" | openssl base64 |'
+           'tr -- "+=/" "-_~"').format(policy)
+    output = subprocess.check_output(cmd, shell=True)
+    return ''.join(output.split('\n'))
+
+
+def get_cloudfront_signature(url, expires,
+                             key_path=CLOUDFRONT_KEY_PATH):
+    policy = get_cloudfront_policy(url, expires)
+    cmd = ('echo \'{0}\' | tr -d "\\n" | openssl sha1 -sign {1} |'
+           ' openssl base64 | tr -- "+=/" "-_~"').format(policy, key_path)
+    output = subprocess.check_output(cmd, shell=True)
+    return ''.join(output.split('\n'))
+
+
+def get_cloudfront_signed_cookie(url, expires):
+    cloudfront_policy = get_cloudfront_policy_base64(url, expires)
+    cloudfront_signature = get_cloudfront_signature(url, expires)
+
+    return (
+        'CloudFront-Signature={0};'
+        'CloudFront-Policy={1};'
+        'CloudFront-Key-Pair-Id={2};'
+    ).format(
+        cloudfront_signature,
+        cloudfront_policy,
+        CLOUDFRONT_KEY_PAIR_ID
+    )
+
+
+def test_flag_cannot_be_gotten_without_signed_cookie():
+    url = '{0}/flag.txt'.format(CLOUDFRONT_URL)
+    r = requests.get(url)
+    assert r.status_code == 403
+
+
+def test_flag_can_be_gotten_flag_with_signed_cookie():
+    url = '{0}/flag.txt'.format(CLOUDFRONT_URL)
+    now = datetime.datetime.now()
+    expires = now + datetime.timedelta(days=1)
+    expires_unixtime = int(time.mktime(expires.timetuple()))
+    cookie = get_cloudfront_signed_cookie(url, expires_unixtime)
+    headers = {'Cookie': cookie}
+    r = requests.get(url, headers=headers)
+    assert r.status_code == 200
+    assert r.text.strip() == 'Catch the flag!'
--- aws/cloudfront/multi_origins/README.md
+++
@@ -1,4 +0,0 @@
-# CloudFront with multi origins
-
-## Author
-[shibataka000](https://github.com/shibataka000)
