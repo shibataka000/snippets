ad3facbb44692bb2cd310e28ac6c1839f4a115d9
shibataka000 <chise.alter.pasta@gmail.com>
2018-07-26 23:53:19 +0900

    Delete azure resource manager template.

--- azure/resource_manager_template/deploy.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-az group create --name ExampleGroup --location "Japan East"
-az group deployment create \
-   --name ExampleDeployment \
-   --resource-group ExampleGroup \
-   --template-file storage.json \
-   --parameters storageAccountType=Standard_GRS
