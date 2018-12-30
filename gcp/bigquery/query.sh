75d3b2202c55bb372ec4460db9e90ba15d14deb2
shibataka000 <chise.alter.pasta@gmail.com>
2018-12-30 18:53:33 +0900

    Use partition in query.

--- gcp/bigquery/query.sh
+++ gcp/bigquery/query.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-QUERY="SELECT distinct userIdentity.arn, userAgent, sourceIPAddress, eventSource, eventName FROM sample.sample order by arn"
+QUERY="SELECT distinct userIdentity.arn, userAgent, sourceIPAddress, eventSource, eventName FROM sample.sample WHERE date(eventTime) = '2018-11-01' ORDER BY arn"
 echo "$QUERY"
 bq --location="US" query --use_legacy=false --dry_run "$QUERY"
 bq --location="US" query --use_legacy=false "$QUERY"
