94c4bffc29ddaac5214e7795c4823c9310330c76
shibataka000 <chise.alter.pasta@gmail.com>
2019-01-26 21:03:35 +0900

    Add snippets to create GKE cluster.

---
+++ gcp/gke/gke.tf
@@ -0,0 +1,22 @@
+provider "google" {
+  project = "numeric-haven-216005"
+}
+
+terraform {
+  backend "gcs" {
+    bucket = "sbtk-tfstate"
+    prefix = "snippets/gcp/gke.tf"
+  }
+}
+
+resource "google_container_cluster" "sandbox" {
+  name = "sandbox"
+  zone = "asia-northeast1-a"
+  initial_node_count = 3
+}
+
+resource "null_resource" "credentials" {
+  provisioner "local-exec" {
+    command = "gcloud container clusters get-credentials ${google_container_cluster.sandbox.name} --zone ${google_container_cluster.sandbox.zone}"
+  }
+}
