ee562c927fea16f31c562ce5748321782e99cfd9
Takao Shibata <chise.alter.pasta@gmail.com>
2021-01-23 10:50:37 +0900

    Add PrivateLink for ECS

--- aws/ecs/fargate/vpc.tf
+++ aws/ecs/fargate/vpc.tf
@@ -91,3 +91,80 @@ resource "aws_route_table_association" "private" {
   route_table_id = aws_route_table.private[count.index].id
 }
 
+# PrivateLink
+
+resource "aws_vpc_endpoint" "ecr-dkr" {
+  vpc_id              = aws_vpc.demo.id
+  service_name        = "com.amazonaws.ap-northeast-1.ecr.dkr"
+  vpc_endpoint_type   = "Interface"
+  subnet_ids          = aws_subnet.private[*].id
+  security_group_ids  = [aws_security_group.vpc_endpoint.id]
+  private_dns_enabled = true
+  tags = {
+    Name = "${var.cluster-name}-ecr-dkr"
+  }
+}
+
+resource "aws_vpc_endpoint" "ecr-api" {
+  vpc_id              = aws_vpc.demo.id
+  service_name        = "com.amazonaws.ap-northeast-1.ecr.api"
+  vpc_endpoint_type   = "Interface"
+  subnet_ids          = aws_subnet.private[*].id
+  security_group_ids  = [aws_security_group.vpc_endpoint.id]
+  private_dns_enabled = true
+  tags = {
+    Name = "${var.cluster-name}-ecr-api"
+  }
+}
+
+resource "aws_vpc_endpoint" "logs" {
+  vpc_id              = aws_vpc.demo.id
+  service_name        = "com.amazonaws.ap-northeast-1.logs"
+  vpc_endpoint_type   = "Interface"
+  subnet_ids          = aws_subnet.private[*].id
+  security_group_ids  = [aws_security_group.vpc_endpoint.id]
+  private_dns_enabled = true
+  tags = {
+    Name = "${var.cluster-name}-logs"
+  }
+}
+
+resource "aws_vpc_endpoint" "s3" {
+  vpc_id            = aws_vpc.demo.id
+  service_name      = "com.amazonaws.ap-northeast-1.s3"
+  vpc_endpoint_type = "Gateway"
+  route_table_ids   = aws_route_table.private[*].id
+  tags = {
+    Name = "${var.cluster-name}-s3"
+  }
+}
+
+resource "aws_security_group" "vpc_endpoint" {
+  vpc_id = aws_vpc.demo.id
+  name   = "${var.cluster-name}-vpc-endpoint"
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port = 0
+    to_port   = 0
+    protocol  = "-1"
+    self      = true
+  }
+
+  ingress {
+    from_port       = 0
+    to_port         = 0
+    protocol        = "-1"
+    security_groups = [aws_security_group.sample.id]
+  }
+
+  tags = {
+    Name = "${var.cluster-name}-vpc-endpoint"
+  }
+}
